c97445d80a36c24ffb807b70e2db777fe1d9009f
Martin Dilger <martin.dilger@axoniq.io>
2026-04-22 09:51:32 +0200

    add revisions

--- payment-api/src/main/java/io/axoniq/demo/bikerental/paymentapi/payment/PaymentPreparedEvent.java
+++ payment-api/src/main/java/io/axoniq/demo/bikerental/paymentapi/payment/PaymentPreparedEvent.java
@@ -1,5 +1,8 @@
 package io.axoniq.demo.bikerental.paymentapi.payment;
 
+import org.axonframework.serialization.Revision;
+
+@Revision("0.0.1")
 public record PaymentPreparedEvent(String paymentId, int amount, String paymentReference) {
 
 }
--- payment-api/src/main/java/io/axoniq/demo/bikerental/paymentapi/payment/PaymentRejectedEvent.java
+++ payment-api/src/main/java/io/axoniq/demo/bikerental/paymentapi/payment/PaymentRejectedEvent.java
@@ -1,5 +1,8 @@
 package io.axoniq.demo.bikerental.paymentapi.payment;
 
+import org.axonframework.serialization.Revision;
+
+@Revision("0.0.1")
 public record PaymentRejectedEvent(String paymentId, String paymentReference) {
 
 }
