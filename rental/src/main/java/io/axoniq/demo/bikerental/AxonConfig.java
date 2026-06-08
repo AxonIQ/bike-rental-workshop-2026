2141628dcc7daea74b610ff5d2cbec9364246d00
Martin Dilger <martin.dilger@axoniq.io>
2026-06-08 16:58:48 +0200

    exercise

--- rental/src/main/java/io/axoniq/demo/bikerental/AxonConfig.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/AxonConfig.java
@@ -1,7 +1,11 @@
 package io.axoniq.demo.bikerental;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.axonframework.messaging.correlation.CorrelationDataProvider;
 import org.axonframework.messaging.correlation.SimpleCorrelationDataProvider;
+import org.axonframework.serialization.Serializer;
+import org.axonframework.serialization.json.JacksonSerializer;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,4 +16,29 @@ public class AxonConfig {
     public CorrelationDataProvider correlationDataProvider() {
         return new SimpleCorrelationDataProvider("dispatchTime");
     }
+
+    @Bean
+    @Qualifier("messageSerializer")
+    public Serializer messageSerializer(ObjectMapper objectMapper) {
+        return JacksonSerializer.builder()
+                .objectMapper(objectMapper)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("eventSerializer")
+    public Serializer eventSerializer(ObjectMapper objectMapper) {
+        return JacksonSerializer.builder()
+                .objectMapper(objectMapper)
+                .build();
+    }
+
+    @Bean
+    @Qualifier("serializer")
+    public Serializer serializer(ObjectMapper objectMapper) {
+        return JacksonSerializer.builder()
+                .objectMapper(objectMapper)
+                .build();
+    }
+
 }
