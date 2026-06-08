fe63cf3348f90b70932918e3968e87a22f9058a9
Martin Dilger <martin.dilger@axoniq.io>
2026-06-08 12:29:28 +0200

    exercise-2-solution

--- rental/src/main/java/io/axoniq/demo/bikerental/ui/RentalController.java
+++ rental/src/main/java/io/axoniq/demo/bikerental/ui/RentalController.java
@@ -3,10 +3,6 @@ package io.axoniq.demo.bikerental.ui;
 import com.google.common.collect.Lists;
 import io.axoniq.demo.bikerental.commands.BikeStatus;
 import io.axoniq.demo.bikerental.commands.RegisterBikeCommand;
-import io.axoniq.demo.bikerental.commands.RequestBikeCommand;
-import io.axoniq.demo.bikerental.commands.ReturnBikeCommand;
-import io.axoniq.demo.bikerental.query.FindAllBikes;
-import io.axoniq.demo.bikerental.query.FindOneBike;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
@@ -49,27 +45,6 @@ public class RentalController {
     @PostMapping("/randomBike")
     public CompletableFuture<String> randomBike(@RequestParam(value = "bikeType") String bikeType) {
         return commandGateway.send(new RegisterBikeCommand(UUID.randomUUID().toString(), bikeType, randomLocation()));
-    }
-
-    @PostMapping("/requestBike")
-    public CompletableFuture<String> requestBike(@RequestParam("bikeId") String bikeId, @RequestParam("renter") String renter) {
-        return commandGateway.send(new RequestBikeCommand(bikeId, renter, UUID.randomUUID().toString()));
-    }
-
-    @PostMapping("/returnBike")
-    public CompletableFuture<String> returnBike(@RequestParam("bikeId") String bikeId, @RequestParam("location") String location) {
-        return commandGateway.send(new ReturnBikeCommand(bikeId, location != null ? location : randomLocation()));
-    }
-
-    @GetMapping("/bikes")
-    public CompletableFuture<List<BikeStatus>> findAll() {
-        var result = queryGateway.query("findAll", new FindAllBikes(), ResponseTypes.multipleInstancesOf(BikeStatus.class));
-        return result;
-    }
-
-    @GetMapping("/bikes/{bikeId}")
-    public CompletableFuture<BikeStatus> findStatus(@PathVariable("bikeId") String bikeId) {
-        return queryGateway.query("findOne", new FindOneBike(bikeId), BikeStatus.class);
     }
 
     private String randomLocation() {
