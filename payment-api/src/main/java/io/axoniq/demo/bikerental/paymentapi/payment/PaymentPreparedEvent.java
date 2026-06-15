87f5df357f78a19688a09b2bd6d19e0660d0c62c
Martin Dilger <martin.dilger@axoniq.io>
2026-06-15 12:01:09 +0200

    prepare dcb training

--- payment-api/src/main/java/io/axoniq/demo/bikerental/paymentapi/payment/PaymentPreparedEvent.java
+++
@@ -1,8 +0,0 @@
-package io.axoniq.demo.bikerental.paymentapi.payment;
-
-import org.axonframework.serialization.Revision;
-
-@Revision("0.0.1")
-public record PaymentPreparedEvent(String paymentId, int amount, String paymentReference) {
-
-}
